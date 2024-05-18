fed145434e138b929b3c1b13a273eb0660542549
padmalatha k <padmalathatce@gmail.com>
2024-05-17 17:19:38 -0700

    Add Basic styles

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Timeline from "../../Component/Timeline/Timeline";
+
+function HomePage() {
+  return (
+    <div>
+      <Timeline />
+    </div>
+  );
+}
+
+export default HomePage;
