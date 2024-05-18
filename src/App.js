861fd46cb0e15a6a426d8e61b165fc4da7a0c8ed
padmalatha k <padmalathatce@gmail.com>
2024-05-17 17:08:30 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-200 p-8">
+      <h1 className="text-3xl font-bold text-blue-700">Hello, Tailwind!</h1>
+      <p className="mt-4 text-gray-600">
+        Welcome to my Tailwind CSS powered React app.
+      </p>
+      <div className="flex flex-col items-center">
+        <div>
+          <a href="<https://vitejs.dev>" target="_blank"></a>
+          <a href="<https://reactjs.org>" target="_blank"></a>
+        </div>
+        <h1 className="bg-red-500">Vite + React</h1>
+      </div>
     </div>
   );
 }
