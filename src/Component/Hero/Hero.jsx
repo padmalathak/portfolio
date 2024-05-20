218158daf98d3c5ee16ce2222b6d69f180d77fdf
padmalatha k <padmalathatce@gmail.com>
2024-05-20 13:21:57 -0700

    Add Name and social Icons

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
 
 function Hero() {
-  return <section>Hero</section>;
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
 }
 
 export default Hero;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import "./HomePage.scss";
 import Timeline from "../../Component/Timeline/Timeline";
 import Hero from "../../Component/Hero/Hero";
+import Navbar from "../../Component/Navbar/Navbar";
 
 function HomePage() {
   return (
