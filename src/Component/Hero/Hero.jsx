7ae1ae4a0018c2372def7ff0c55b7cbe58ed833d
padmalatha k <padmalathatce@gmail.com>
2024-05-21 11:33:58 -0700

    Hero banner

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import "./Hero.scss";
 
+import scroll from "../../assets/icons/scroll.png";
+import passport from "../../assets/imgs/passport.png";
+
 function Hero() {
   return (
     <section id="Homepage">
       <Navbar />
-      <div className="hero">Hero</div>
+      <div className="hero">
+        <div>
+          <img src={passport} alt="Hero" className="hero__img" />
+        </div>
+        <div className="hero__text">
+          <h1 className="hero__title">PADMA - Software Developer</h1>
+          <p className="hero__description">
+            Dedicated and solutions-oriented Software development engineer with
+            3 years of experience in full software development life cycle.
+          </p>
+          <div className="hero__buttons">
+            <button className="hero__button">See the latest Works</button>
+            <button className="hero__button">Contact Me</button>
+          </div>
+          <img src={scroll} alt="scroll" className="hero__scroll" />
+        </div>
+      </div>
     </section>
   );
 }
