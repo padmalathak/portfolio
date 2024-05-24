32a10d4468f2e654d587e6c318b606c6fa910858
padmalatha k <padmalathatce@gmail.com>
2024-05-24 15:48:11 -0700

    Timeline

--- src/Component/Timeline/Timeline.jsx
+++ src/Component/Timeline/Timeline.jsx
@@ -1,6 +1,16 @@
 import "./Timeline.scss";
+import { motion } from "framer-motion";
 
 function Timeline() {
+  const variants = {
+    visible: { opacity: 1, x: -30, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, x: -200 },
+  };
+
+  const rightVariants = {
+    visible: { opacity: 1, x: 30, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, x: 200 },
+  };
   return (
     <section id="Experience">
       <div className="timelines">
@@ -8,20 +18,41 @@ function Timeline() {
         <div class="container">
           <div class="timeline">
             <ul>
-              <li>
+              <motion.li
+                className="timeline__list"
+                variants={variants}
+                initial="hidden"
+                whileInView="visible"
+              >
                 <div class="timeline-content">
-                  <h3 class="date">10th may, 2024</h3>
-                  <h1>BrainStation</h1>
+                  <h3 class="date">10th may, 2024 - 95%</h3>
+                  <h1>BrainStation-Software Engineering Student</h1>
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consectetur tempora ab laudantium voluptatibus aut eos
-                    placeat laborum, quibusdam exercitationem labore.
+                    Gained hands-on experience in full-stack development through
+                    learning and applying:
+                    <ul>
+                      <li>HTML, CSS, JavaScript, React, Node.js, MySQL </li>
+                      <li className="list-disc">
+                        Built and deployed multi-page web applications
+                      </li>
+                      <li className="list-disc">
+                        Collaborated effectively with other developers
+                      </li>
+                      <li className="list-disc">
+                        Worked with databases to manage and manipulate data
+                      </li>
+                    </ul>{" "}
                   </p>
                 </div>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li
+                className="timeline__list right"
+                variants={rightVariants}
+                initial="hidden"
+                whileInView="visible"
+              >
                 <div class="timeline-content">
-                  <h3 class="date">20th may, 2010</h3>
+                  <h3 class="date">20th may, 2010 </h3>
                   <h1>Heading 2</h1>
                   <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -29,8 +60,13 @@ function Timeline() {
                     placeat laborum, quibusdam exercitationem labore.
                   </p>
                 </div>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li
+                className="timeline__list"
+                variants={variants}
+                initial="hidden"
+                whileInView="visible"
+              >
                 <div class="timeline-content">
                   <h3 class="date">20th may, 2010</h3>
                   <h1>Heading 3</h1>
@@ -40,8 +76,13 @@ function Timeline() {
                     placeat laborum, quibusdam exercitationem labore.
                   </p>
                 </div>
-              </li>
-              <li>
+              </motion.li>
+              <motion.li
+                className="timeline__list right"
+                variants={rightVariants}
+                initial="hidden"
+                whileInView="visible"
+              >
                 <div class="timeline-content">
                   <h3 class="date">20th may, 2010</h3>
                   <h1>Heading 4</h1>
@@ -51,7 +92,7 @@ function Timeline() {
                     placeat laborum, quibusdam exercitationem labore.
                   </p>
                 </div>
-              </li>
+              </motion.li>
             </ul>
           </div>
         </div>
