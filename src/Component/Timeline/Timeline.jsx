650fe5301bf4a4a1e4e77287d594d4437a220f07
padmalatha k <padmalathatce@gmail.com>
2024-05-23 17:11:34 -0700

    Timeline

--- src/Component/Timeline/Timeline.jsx
+++ src/Component/Timeline/Timeline.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import "./Timeline.scss";
 
 function Timeline() {
-  return <section id="Experience">Experience</section>;
+  return (
+    <section id="Experience">
+      <div className="timelines">
+        <h3 className="timelines__heading">My Timeline</h3>
+        <div class="container">
+          <div class="timeline">
+            <ul>
+              <li>
+                <div class="timeline-content">
+                  <h3 class="date">10th may, 2024</h3>
+                  <h1>BrainStation</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur tempora ab laudantium voluptatibus aut eos
+                    placeat laborum, quibusdam exercitationem labore.
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-content">
+                  <h3 class="date">20th may, 2010</h3>
+                  <h1>Heading 2</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur tempora ab laudantium voluptatibus aut eos
+                    placeat laborum, quibusdam exercitationem labore.
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-content">
+                  <h3 class="date">20th may, 2010</h3>
+                  <h1>Heading 3</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur tempora ab laudantium voluptatibus aut eos
+                    placeat laborum, quibusdam exercitationem labore.
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-content">
+                  <h3 class="date">20th may, 2010</h3>
+                  <h1>Heading 4</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Consectetur tempora ab laudantium voluptatibus aut eos
+                    placeat laborum, quibusdam exercitationem labore.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Timeline;
