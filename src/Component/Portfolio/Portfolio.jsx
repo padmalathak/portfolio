fa535f5fd6db159b5089f606c06b0d0e0864d79f
padmalatha k <padmalathatce@gmail.com>
2024-05-28 12:11:47 -0700

    Contain the basic structure for portfolio

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -19,23 +19,84 @@ const draw = {
 function Portfolio() {
   return (
     <section id="Portfolio" className="Portfolio">
-      {" "}
-      <motion.svg
-        width="600"
-        height="600"
-        viewBox="0 0 600 600"
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.circle
-          cx="100"
-          cy="100"
-          r="80"
-          stroke="#ff0055"
-          variants={draw}
-          custom={1}
-        />{" "}
-      </motion.svg>
+      <motion.div className="services">
+        <motion.div className="textContainer">
+          <p>
+            I focus on helping your brand grow
+            <br /> and move forward
+          </p>
+          <hr />
+        </motion.div>
+        <motion.div className="titleContainer">
+          <div className="title">
+            <img src="/people.webp" alt="" />
+            <h1>
+              <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
+            </h1>
+          </div>
+          <div className="title">
+            <h1>
+              <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
+              Business.
+            </h1>
+            <button>WHAT WE DO?</button>
+          </div>
+        </motion.div>
+        <motion.div className="listContainer">
+          <motion.div
+            className="box"
+            whileHover={{ background: "lightgray", color: "black" }}
+          >
+            <h2>Branding</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              libero enim nisi aliquam consectetur expedita magni eius ex
+              corrupti animi! Ad nam pariatur assumenda quae mollitia libero
+              repellat explicabo maiores?
+            </p>
+            <button>Go</button>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ background: "lightgray", color: "black" }}
+          >
+            <h2>Branding</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              libero enim nisi aliquam consectetur expedita magni eius ex
+              corrupti animi! Ad nam pariatur assumenda quae mollitia libero
+              repellat explicabo maiores?
+            </p>
+            <button>Go</button>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ background: "lightgray", color: "black" }}
+          >
+            <h2>Branding</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              libero enim nisi aliquam consectetur expedita magni eius ex
+              corrupti animi! Ad nam pariatur assumenda quae mollitia libero
+              repellat explicabo maiores?
+            </p>
+            <button>Go</button>
+          </motion.div>
+          <motion.div
+            className="box"
+            whileHover={{ background: "lightgray", color: "black" }}
+          >
+            <h2>Branding</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              libero enim nisi aliquam consectetur expedita magni eius ex
+              corrupti animi! Ad nam pariatur assumenda quae mollitia libero
+              repellat explicabo maiores?
+            </p>
+            <button>Go</button>
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
