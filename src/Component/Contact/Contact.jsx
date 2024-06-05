f8348771e8c025ad67d906a54a200636f4804e7f
padmalatha k <padmalathatce@gmail.com>
2024-06-05 15:48:35 -0700

    Styles for the page

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -7,10 +7,10 @@ function Contact() {
           <h1>Let's Create Magic Together!</h1>
           <div className="item">
             <h2>Mail</h2>
-            <span>padmalatha</span>
+            <span>padmalathalakshmi@gmail.com</span>
           </div>
           <div className="item">
-            <h2>Adress</h2>
+            <h2>Address</h2>
             <span>Langley,BC</span>
           </div>
           <div className="item">
@@ -18,14 +18,14 @@ function Contact() {
             <span>+16047247962</span>
           </div>
         </div>
-      </div>
-      <div className="formContainer">
-        <form>
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Email" />
-          <textarea rows={8} placeholder="Message" />
-          <button>Submit</button>
-        </form>
+        <div className="formContainer">
+          <form>
+            <input type="text" required placeholder="Name" />
+            <input type="email" required placeholder="Email" />
+            <textarea rows={8} placeholder="Message" />
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
     </section>
   );
