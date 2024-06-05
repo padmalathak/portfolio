43d4f6d16b287d6013747a6506f241ece7cb0d27
padmalatha k <padmalathatce@gmail.com>
2024-06-05 15:23:35 -0700

    Basic Structire

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,6 +1,26 @@
 import "./Contact.scss";
 function Contact() {
-  return <section id="Contact">Contact</section>;
+  return (
+    <section id="Contact">
+      <div className="contact">
+        <div className="textContainer">
+          <h1>Let's Create Magic Together!</h1>
+          <div className="item">
+            <h2>Mail</h2>
+            <span>padmalatha</span>
+          </div>
+          <div className="item">
+            <h2>Mail</h2>
+            <span>padmalatha</span>
+          </div>
+          <div className="item">
+            <h2>Mail</h2>
+            <span>padmalatha</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
