a4ced02842cf07e6016d8ab80f1836e1a928d8ac
padmalatha k <padmalathatce@gmail.com>
2024-06-05 16:46:04 -0700

    Contact page

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,23 +1,47 @@
+import { animate, motion } from "framer-motion";
 import "./Contact.scss";
+
+const variants = {
+  initial: {
+    y: 500,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
 function Contact() {
   return (
     <section id="Contact">
-      <div className="contact">
-        <div className="textContainer">
-          <h1>Let's Create Magic Together!</h1>
-          <div className="item">
+      <motion.div
+        className="contact"
+        variants={variants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true }}
+      >
+        <motion.div className="textContainer" variants={variants}>
+          <motion.h1 variants={variants}>
+            Let's Create Magic Together!
+          </motion.h1>
+          <motion.div variants={variants} className="item">
             <h2>Mail</h2>
             <span>padmalathalakshmi@gmail.com</span>
-          </div>
-          <div className="item">
+          </motion.div>
+          <motion.div variants={variants} className="item">
             <h2>Address</h2>
             <span>Langley,BC</span>
-          </div>
-          <div className="item">
+          </motion.div>
+          <motion.div variants={variants} className="item">
             <h2>Phone</h2>
             <span>+16047247962</span>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
         <div className="formContainer">
           <form>
             <input type="text" required placeholder="Name" />
@@ -26,7 +50,7 @@ function Contact() {
             <button>Submit</button>
           </form>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
