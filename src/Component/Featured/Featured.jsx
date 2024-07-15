28f4842553a1848f4c550ec4c58c23d5ab415f38
Padma <padmalathatce@gmail.com>
2024-07-15 16:48:37 -0700

    TripPilot

--- src/Component/Featured/Featured.jsx
+++ src/Component/Featured/Featured.jsx
@@ -31,9 +31,11 @@ const items = [
   },
   {
     id: 4,
-    title: "TripPilot",
+    title: "TripPilot - In Progress",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: `Are you still using MSExcel to organize your trip information? What if there is an app feature that organizes all your travel info in one place? Then, you are all
+    set to explore your dream destination with TripPilot. TripPilot is a cutting edge travel planning app that is designed to assist you in
+    organizing, managing, and cooperating on tasks, and information when planning a trip.Tech Stack : React,TypeScript, Node and MongoDB`,
   },
 ];
 
